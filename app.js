d3fd7f840a9bb23f4f77de69c25807198d553cfd
Anna Marfel <anna.marfel@gmail.com>
2018-01-15 19:59:06 +0300

    array args is used directly -arr

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
     'use strict';
 
     function printWordByWord(...args) {
-        let arr = [...args];
-        let words = arr.pop().split(' ');
+        let words = args.pop().split(' ');
         let time = 0,
             currentTime;
 
         for (let i = 0; i < words.length; i++) {
-            currentTime = i >= arr.length ? arr[arr.length - 1] : arr[i];
+            currentTime = i >= args.length ? args[args.length - 1] : args[i];
             time = time + currentTime;
             setTimeout(() => {
                 console.log(words[i]);
             }, time * 1000)
         }
     }
+    printWordByWord(0.3, 0.3, 0.3, 1, 'Карл у Клары украл кораллы и кларнет');
 
 }());
